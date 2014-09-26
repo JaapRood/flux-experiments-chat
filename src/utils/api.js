742e526b64c7c9be3e9a98b95de2633414fa20e3
Jaap Rood <jaap@jaaprood.nl>
2014-09-26 11:04:51 +1000

    Added a very simple api util

---
+++ src/utils/api.js
@@ -0,0 +1,45 @@
+var MessagesActions = require('app/action-creators/messages');
+
+// !!! Please Note !!!
+// We are using localStorage as an example, but in a real-world scenario, this
+// would involve XMLHttpRequest, or perhaps a newer client-server protocol.
+// The function signatures below might be similar to what you would build, but
+// the contents of the functions are just trying to simulate client-server
+// communication and server-side processing.
+
+module.exports = {
+
+  getAllMessages: function(context) {
+    // simulate retrieving data from a database
+    var rawMessages = JSON.parse(localStorage.getItem('messages'));
+
+
+
+    // simulate success callback
+    context.executeAction(MessagesActions.receiveAll, rawMessages);
+  },
+
+  createMessage: function(context, message, threadName) {
+    // simulate writing to a database
+    var rawMessages = JSON.parse(localStorage.getItem('messages'));
+    var timestamp = Date.now();
+    var id = 'm_' + timestamp;
+    var threadID = message.threadID || ('t_' + Date.now());
+    var createdMessage = {
+      id: id,
+      threadID: threadID,
+      threadName: threadName,
+      authorName: message.authorName,
+      text: message.text,
+      timestamp: timestamp
+    };
+    rawMessages.push(createdMessage);
+    localStorage.setItem('messages', JSON.stringify(rawMessages));
+
+    // simulate success callback
+    setTimeout(function() {
+      context.executeAction(MessagesActions.receiveCreatedMessage, createdMessage);
+    }, 0);
+  }
+
+};
