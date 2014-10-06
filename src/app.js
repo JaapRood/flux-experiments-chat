1db09c3866d4948017e509b1fa6bf8c2d942c586
Jaap Rood <jaap@jaaprood.nl>
2014-10-06 12:34:27 +1000

    Opening a thread on app launch

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 var React = require('react'),
 	ChatApp = require('./components/ChatApp.react'),
 	FluxContext = require('./lib/flux-context'),
-	Stores = require('./stores');
+	Stores = require('./stores'),
+	StoreNames = require('./store-names'),
+	ThreadsActions = require('./action-creators/threads');
 
 var ExampleData = require('./example-data'),
 	Api = require('./utils/api');
@@ -14,6 +16,11 @@ var context = new FluxContext({
 ExampleData.init();
 Api.getAllMessages(context.componentContext);
 
+// this should probably go else where, but we'll keep  it here for now
+
+var allThreads = context.dispatcher.getStore(StoreNames.THREADS).getAll();
+var firstThread = allThreads.first();
+ThreadsActions.clickThread(context, firstThread.get('id'));
 
 document.addEventListener('DOMContentLoaded', function() {
 
