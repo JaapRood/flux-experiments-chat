e70a49c2111c1bb48f9a51168da48bbc586b3c95
Jaap Rood <jaap@jaaprood.nl>
2014-10-20 21:05:31 +1000

    Separated all the chat specific code into a chat app plugin, leaving the actual app.js pretty general, which is hopefully an indication of how with Bly we can create pluggable bits of app

--- src/app.js
+++ src/app.js
@@ -1,51 +1,25 @@
 var React = require('react'),
-	ChatApp = require('./components/ChatApp.react'),
-	Stores = require('./stores'),
-	StoreNames = require('./store-names'),
-	ThreadsActions = require('./action-creators/threads'),
-	MessagesActions = require('./action-creators/messages'),
-	Bly = require('app/lib/bly');
-
-var ExampleData = require('./example-data'),
-	Api = require('./utils/api');
-
-// populate with debug data
-ExampleData.init();
-
+	Bly = require('app/lib/bly'),
+	ChatApp = require('./chatapp');
 
 var app = new Bly.App();
 
 app.register([
-	Stores
+	ChatApp
 ], function(err) {
 	if (err) throw err; // treat errors registering plugins as unrecoverable event
 
 	app.start();
-	
-	Api.getAllMessages(function(err, rawMessages) {
-		if (err) return console.error(err);
 
-		MessagesActions.receiveAll(app, rawMessages)
+	// as soon as the DOM is ready we'll start rendering the app
+	document.addEventListener('DOMContentLoaded', function() {
+		app.render(function(results) {
+			React.renderComponent(
+				results.chat,
 
-		var allThreads = app.stores(StoreNames.THREADS).getAll();
-		var firstThread = allThreads.first();
-
-		ThreadsActions.clickThread(app, firstThread.get('id'));
-	});
-});
-
-
-
-// as soon as the DOM is ready we'll start rendering the app
-document.addEventListener('DOMContentLoaded', function() {
-	app.render(function() {
-		React.renderComponent(
-			ChatApp({
-				app: app
-			}),
-
-			document.getElementById('react')
-		);
+				document.getElementById('react')
+			);
+		});
 	});
 });
 
---
+++ src/chatapp.js
@@ -0,0 +1,44 @@
+var ChatApp = require('./components/ChatApp.react'),
+	Stores = require('./stores'),
+	StoreNames = require('./store-names'),
+	ThreadsActions = require('./action-creators/threads'),
+	MessagesActions = require('./action-creators/messages'),
+	Api = require('./utils/api'),
+	ExampleData = require('./example-data');
+
+// populate with debug data
+ExampleData.init();
+
+exports.name = 'chatapp';
+
+exports.register = function(plugin, options, next) {
+
+	plugin.register([
+		Stores
+	], function(err) {
+		if (err) return next(err); // treat errors registering plugins as unrecoverable event
+
+		plugin.after(function() {
+
+			Api.getAllMessages(function(err, rawMessages) {
+				if (err) return console.error(err);
+
+				MessagesActions.receiveAll(plugin, rawMessages)
+
+				var allThreads = plugin.stores(StoreNames.THREADS).getAll();
+				var firstThread = allThreads.first();
+
+				ThreadsActions.clickThread(plugin, firstThread.get('id'));
+			});
+		});
+
+		// render the chat app
+		plugin.results(function(report) {
+			report('chat', ChatApp({
+				app: plugin
+			}));
+		});
+
+		next();
+	});
+}
