589d462b73a6445efdf208656b8cac1de4ef3a4d
Jaap Rood <jaap@jaaprood.nl>
2014-10-20 16:41:50 +1000

    Making sure everything gets registered before calling next on the stores plugin

--- src/stores.js
+++ src/stores.js
@@ -7,40 +7,41 @@ var stores = [
 
 exports.register = function(plugin, options, next) {
 	// stores can define themselves as a plugin
-	// ----------------------------------------
-	
 	plugin.register([
 		require('./stores/messages')
-	], next);
+	], function(err) {
+		if (err) return next(err);
+		// or we can help them do it
 
-	// or we can help them do it
-	// -------------------------
+		// create instance for every store
+		var storeInstances = _.map(stores, function(storeConstructor) {
+			return new storeConstructor(plugin.stores);
+		});
 
-	// create instance for every store
-	var storeInstances = _.map(stores, function(storeConstructor) {
-		return new storeConstructor(plugin.stores);
-	});
+		// index by name
+		var storesByName = _.indexBy(storeInstances, function(store) {
+			return store.constructor.storeName || _.uniqueId('store_');
+		});
 
-	// index by name
-	var storesByName = _.indexBy(storeInstances, function(store) {
-		return store.constructor.storeName || _.uniqueId('store_');
-	});
+		// register handlers
+		var actionHandlers = _.map(storesByName, function(store, name) {
+			var handlers = store.constructor.handlers;
 
-	// register handlers
-	var actionHandlers = _.map(storesByName, function(store, name) {
-		var handlers = store.constructor.handlers;
-
-		return _.map(handlers, function(handler, actionName) {
-			plugin.action({
-				name: actionName,
-				handler: store[handler].bind(store),
-				ref: name
+			return _.map(handlers, function(handler, actionName) {
+				plugin.action({
+					name: actionName,
+					handler: store[handler].bind(store),
+					ref: name
+				});
 			});
 		});
+
+		// make stores available to rest of the app
+		plugin.stores(storesByName);
+			
+		next();
 	});
 
-	// make stores available to rest of the app
-	plugin.stores(storesByName);
 
 
 }
